06489180dadfba2f5114f5e9ba9387faffc0c0e4
Pedro Midueño <pedro.midueno@gmail.com>
2023-07-10 17:01:17 -0600

    Preparing for dockerize

--- films/src/controllers/getAllFilms.js
+++ films/src/controllers/getAllFilms.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const { response } = require('../utils')
 
 module.exports = async (req, res) => {
-    const axiosResponse = await axios('http://localhost:8004/Film')
+    const axiosResponse = await axios('http://database:8004/Film')
     const films = axiosResponse.data
     response(res, 200, films)
 }

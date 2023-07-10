e6662be556414e2f11e6aad2d36ffad31936be87
Pedro Midueño <pedro.midueno@gmail.com>
2023-07-10 11:43:02 -0600

    Implemented http request to database in getCharacter method

--- characters/src/controllers/getCharacters.js
+++ characters/src/controllers/getCharacters.js
@@ -1,7 +1,9 @@
-const Character = require('../data')
+// const Character = require('../data')
 const { response } = require('../utils')
+const axios = require('axios')
 
 module.exports = async (req, res) => {
-    const characters = await Character.list()
+    const axiosResponse = await axios('http://database:8004/Character')
+    const characters = axiosResponse.data
     response(res, 200, characters)
 }

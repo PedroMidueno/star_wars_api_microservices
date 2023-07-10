17d1cbe50d308dcdb32f4de596446968aaa0bf80
Pedro Midueño <pedro.midueno@gmail.com>
2023-07-10 17:00:36 -0600

    Preparing to dockerize

--- characters/src/controllers/getCharacterById.js
+++ characters/src/controllers/getCharacterById.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 
 module.exports = async (req, res) => {
     const { id } = req.params
-    const axiosResponse = await axios('http://localhost:8004/Character/' + id)
+    const axiosResponse = await axios('http://database:8004/Character/' + id)
     const char = axiosResponse.data
     response(res, 200, char || 'Character does not exist in database')
 }
